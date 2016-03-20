bbf762f614f5e77f770dd669840234070c3599fe
ushiboy <ushiboy.dev@gmail.com>
2016-03-20 14:28:46 +0900

    add model validate

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,8 +1,10 @@
 <?php
 namespace ToyToyToy\Model;
 
-use \Illuminate\Database\Eloquent;
+use Illuminate\Database\Eloquent;
 use ToyToyToy\Mixin\HasSecurePassword;
+use Respect\Validation\Validator as v;
+
 
 class User extends Eloquent\Model
 {
@@ -18,5 +20,21 @@ class User extends Eloquent\Model
     protected function getPasswordDigest()
     {
         return $this->password_digest;
+    }
+
+    public function validate()
+    {
+        $userValidator = v::attribute('name', v::stringType()->length(null, 100))
+            ->attribute('email', v::email());
+        $obj = new \stdClass;
+        $obj->name = $this->name;
+        $obj->email = $this->email;
+        $userValidator->assert($obj);
+    }
+
+    public function save(array $options = [])
+    {
+        $this->validate();
+        return parent::save($options);
     }
 }
